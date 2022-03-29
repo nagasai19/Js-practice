e4f9159839403bbdf9169f1fdcbe00776cc121a4
nagasai <nagasai@Nagasais-MacBook-Pro.local>
2022-03-29 09:55:33 +0530

    Changes to files

--- practice-snippets/currying.js
+++ practice-snippets/currying.js
@@ -1,62 +1,48 @@
-//code owner : Nagasai Mallela
 
-//Definition : 
+//Currying is a transformation of functions
+//that translates a function from callable as f(a, b, c) 
+//into callable as f(a)(b)(c).
 
+//Currying doesn’t call a function. It just transforms it.
 
-//currying is a process  of  converting a function 
+function curry(functionName){
+    return function(a){
+        return function(b){
+            return functionName(a,b)
+        }
+    }
+}
 
-//that takes multiple arguments and then converting 
+function sum(a,b,c){
+    return a+b+c
+}
 
-//into one or more function
+function multiply(a,b,c){
+    return a*b*c
+}
 
-//that takes one argument at a time instead.
+function curry(functionName){
+   return function curried(...args){
+       if(functionName.length <=args.length){
+           return functionName.apply(this,args)
+       }else{
+           return function(...args2){
+               return curried.apply(this,args.concat(args2))
+           }
+       }
+   }
+}
 
+let curriedSum = curry(sum); 
+let curriedMulti= curry(multiply)
 
-//const add = (x, y) => x + y; --- normal fn
-
-//const add = x => y => x + y;  ---currying fn call 
-
-
-//add(10, 5);  // 15 --- normal fn call
-
-//uses of currying functions : you can call them later with arguments 
-//const add10 = add(10);  --- currying call.
-//add10(5);  // 15
-
-
-// function curry(func) {
-//     return function curried(...args) {
-//       if (args.length >= func.length) {
-//         return func.apply(this, args);
-//       } else {
-//         return function (...args2) {
-//           return curried.apply(this, args.concat(args2));
-//         }
-//       }
-//     };
-  
-//   }
-  
-//   function sum(a, b, c) {
-//     return a + b + c;
-//   }
-  
-//   function mul(a,b,c){
-//     return a*b*c
-//   }
-  
-  
-//   let curriedSum = curry(sum);
-  
-//   let curriedSum1 = curry(mul);
-  
-//   console.log(curriedSum)
-  
-//   //console.log((curriedSum(1, 2, 3)),'1111') //still callable normally
-  
-//   //console.log((curriedSum1(1)(2, 15)),'2222') //currying of 1st arg
-  
-//   console.log(curriedSum(1)(2)(3)) // full currying
+console.log(curriedSum(1,2,3))
+console.log(curriedSum(1)(2,3))
+console.log(curriedSum(1)(2)(3))
+console.log(curriedMulti(1)(2)(3))
+console.log(curriedMulti(1,2,3))
+console.log(curriedMulti(1)(2,3))
+console.log(curriedMulti(1)(2)(4))
 
 
 
