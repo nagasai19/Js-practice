a30e90024f0bda846684eac86122c8b0a5fcc518
Nagasai  Mallela <nagasaim19@gmail.com>
2022-07-26 02:28:33 +0530

    Added Keys

--- practice-snippets/Leetcode/ContainsDuplicate.js
+++ practice-snippets/Leetcode/ContainsDuplicate.js
@@ -1,40 +1,38 @@
 //https://leetcode.com/problems/contains-duplicate/
 
-//CMD + K + Q key 
-
+//CMD + K + Q keys
 
 //Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
-const arrayy =[1,2,3,4]
+const arrayy = [1, 2, 3, 4];
 
-function hasElementinMap(collection,element) {
-    return collection.has(element)
+function hasElementinMap(collection, element) {
+  return collection.has(element);
 }
 
-function addElementMap(collection,element){
-    return collection.add(element,1)
+function addElementMap(collection, element) {
+  return collection.add(element, 1);
 }
 
-function ContainsDuplicate(arrayy){
-  let firstPointer = 0 ;
-  let secondPointer =arrayy.length-1;
-  let nonDuplicatesCollections = new Set()
-  while(firstPointer < secondPointer){
-     let startElement = arrayy[firstPointer]
-     let endElement = arrayy[secondPointer]
-     if(hasElementinMap(nonDuplicatesCollections,startElement)){
-         return true
-     }
-     addElementMap(nonDuplicatesCollections,startElement)
-     if(hasElementinMap(nonDuplicatesCollections,endElement)){
-        return true
+function ContainsDuplicate(arrayy) {
+  let firstPointer = 0;
+  let secondPointer = arrayy.length - 1;
+  let nonDuplicatesCollections = new Set();
+  while (firstPointer < secondPointer) {
+    let startElement = arrayy[firstPointer];
+    let endElement = arrayy[secondPointer];
+    if (hasElementinMap(nonDuplicatesCollections, startElement)) {
+      return true;
     }
-    addElementMap(nonDuplicatesCollections,endElement)
+    addElementMap(nonDuplicatesCollections, startElement);
+    if (hasElementinMap(nonDuplicatesCollections, endElement)) {
+      return true;
+    }
+    addElementMap(nonDuplicatesCollections, endElement);
     firstPointer++;
     secondPointer--;
   }
   return false;
-
 }
 
-ContainsDuplicate(arrayy)
+ContainsDuplicate(arrayy);
